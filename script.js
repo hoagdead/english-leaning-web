7074cc8c7817bb5e43f73b6af58214eada30360a
hikari <95992625+hoagdead@users.noreply.github.com>
2024-03-03 17:35:54 +0700

    new sign up form

--- script.js
+++ script.js
@@ -25,3 +25,40 @@ function displayResults(data) {
         searchResults.innerHTML = "<p>No results found</p>";
     }
 }
+// sign up
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+// Add 'show' class to home element when the page loads
+document.addEventListener("DOMContentLoaded", function() {
+  home.classList.add("show");
+});
+
+
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
