e9a00ddc954b385038229bff505e4b7af1712892
hikari <95992625+hoagdead@users.noreply.github.com>
2024-03-10 11:58:52 +0700

    update dictionary.html

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const formOpenBtn = document.querySelector("#form-open"),
-  home = document.querySelector(".home"),
-  formContainer = document.querySelector(".form_container"),
-  formCloseBtn = document.querySelector(".form_close"),
-  signupBtn = document.querySelector("#signup"),
-  loginBtn = document.querySelector("#login"),
-  pwShowHide = document.querySelectorAll(".pw_hide");
+home = document.querySelector(".home"),
+formContainer = document.querySelector(".form_container"),
+formCloseBtn = document.querySelector(".form_close"),
+signupBtn = document.querySelector("#signup"),
+loginBtn = document.querySelector("#login"),
+pwShowHide = document.querySelectorAll(".pw_hide");
 
 formOpenBtn.addEventListener("click", () => home.classList.add("show"));
 formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
